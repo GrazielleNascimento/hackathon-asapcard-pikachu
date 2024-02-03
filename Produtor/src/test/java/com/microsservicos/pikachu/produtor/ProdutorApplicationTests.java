cf19bb0ea66b52e21f7dd53e7e0aa2a98ee7ec50
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-02-03 11:56:01 -0300

    Inicializacao do Projeto

---
+++ Produtor/src/test/java/com/microsservicos/pikachu/produtor/ProdutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsservicos.pikachu.produtor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
